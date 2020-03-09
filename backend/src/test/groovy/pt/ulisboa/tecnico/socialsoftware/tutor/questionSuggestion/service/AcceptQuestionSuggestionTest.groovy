034b20cf1da5a161de0f1cf0895ef53691e0cbc1
Miguel Leve <miguel.levezinho@tecnico.ulisboa.pt>
2020-03-09 17:20:16 +0000

    issue #6 Done
    - Added file AcceptQuestionSuggestionTest.groovy
       - Defined 3 new tests
    - Added file RejectQuestionSuggestionTest.groovy
       - Defined 3 new tests
    
    - Fixed wrong package name

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/questionSuggestion/service/AcceptQuestionSuggestionTest.groovy
@@ -0,0 +1,74 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.questionSuggestion.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecution
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecutionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.ImageDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.OptionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.questionSuggestion.dto.QuestionSuggestionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.questionSuggestion.repository.QuestionSuggestionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.questionSuggestion.QuestionSuggestionService
+import pt.ulisboa.tecnico.socialsoftware.tutor.questionSuggestion.domain.QuestionSuggestion
+import spock.lang.Specification
+
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.INVALID_NULL_ARGUMENTS
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.USER_IS_TEACHER
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.USER_NOT_FOUND
+
+@DataJpaTest
+class AcceptQuestionSuggestionTest extends Specification {
+
+    @Autowired
+    QuestionSuggestionService questionSuggestionService
+
+    @Autowired
+    CourseExecutionRepository courseExecutionRepository
+
+    @Autowired
+    CourseRepository courseRepository
+
+    @Autowired
+    QuestionSuggestionRepository questionSuggestionRepository
+
+    @Autowired
+    UserRepository userRepository
+
+    def question
+    def questionSuggestion
+
+    def setup() {
+    }
+
+    def "The question suggestion exists and a teacher accepts it"() {
+        // The question becomes available
+        expect: false
+    }
+
+    def "The question suggestion exists and a teacher from a different course accepts it"() {
+        // An exception is thrown
+        expect: false
+    }
+
+    def "The question suggestion exists and a student accepts it"() {
+        // An exception is thrown
+        expect: false
+    }
+
+    @TestConfiguration
+    static class QuestionSuggestionServiceImplTestContextConfiguration {
+
+        @Bean
+        QuestionSuggestionService questionSuggestionService() {
+            return new QuestionSuggestionService()
+        }
+    }
+}
