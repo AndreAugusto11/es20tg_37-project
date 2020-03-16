321b32b3e5f81f6088cf4b32a22881fa4ad2ff39
Mascarenhas12 <manuel.d.mascarenhas@tecnico.ulisboa.pt>
2020-03-16 18:56:55 +0000

    Issue #51 done

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/api/TournamentController.java
@@ -0,0 +1,26 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentService;
+import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.dto.TournamentDto;
+
+import java.util.List;
+
+@RestController
+public class TournamentController {
+    private static Logger logger = LoggerFactory.getLogger(TournamentController.class);
+
+    @Autowired
+    private TournamentService tournamentService;
+
+    @GetMapping("/executions/{executionId}/tournaments")
+    @PreAuthorize("hasPermission(#executionId,'EXECUTION.ACCESS')")
+    public List<TournamentDto> getTournaments() {
+        return tournamentService.getTournaments();
+    }
+}
