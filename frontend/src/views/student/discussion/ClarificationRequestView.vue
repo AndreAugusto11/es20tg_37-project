6ffa4d2070cac7f571e5f45bf8fa08a6a93b4c53
AndreAugusto <andre.9a@gmail.com>
2020-04-14 14:20:04 +0100

    Adding answer to student Request. Missing encapsulation in Component

--- frontend/src/views/student/discussion/ClarificationRequestView.vue
+++ frontend/src/views/student/discussion/ClarificationRequestView.vue
@@ -1,11 +1,45 @@
 <template>
-    <clarification-request :clarification-request="clarificationRequest" />
+    <div>
+        <clarification-request :clarification-request="clarificationRequest" />
+        <v-card
+                v-if="this.clarificationRequest.clarificationRequestAnswerDto.content"
+                class="mx-auto mt-10"
+                max-width="1000"
+                outlined
+        >
+
+            <v-row
+                    align="center"
+                    class="spacer ml-5 mt-5"
+                    no-gutters
+            >
+                <v-col
+                        sm="2"
+                        md="9"
+                >
+                    <h2 class="mb-1 post-text">{{ this.clarificationRequest.clarificationRequestAnswerDto.name }}</h2>
+                </v-col>
+            </v-row>
+
+            <v-row
+                    align="center"
+                    class="spacer ml-5 mb-5"
+                    no-gutters
+            >
+                <v-col class="post-text">
+                    <span v-html="convertMarkDownNoFigure(this.clarificationRequest.clarificationRequestAnswerDto.content, null)" />
+                </v-col>
+            </v-row>
+        </v-card>
+    </div>
 </template>
 
 <script lang="ts">
   import { Component, Vue } from 'vue-property-decorator';
+  import { convertMarkDownNoFigure } from '@/services/ConvertMarkdownService';
   import { ClarificationRequest } from '@/models/discussion/ClarificationRequest';
   import ClarificationRequestComponent from '@/components/discussion/ClarificationRequestComponent.vue';
+  import Image from "@/models/management/Image";
 
   @Component({
     components: {
@@ -18,6 +52,10 @@
     created() {
         this.clarificationRequest = new ClarificationRequest(JSON.parse(this.$route.params.clarificationRequest));
     }
+
+      convertMarkDownNoFigure(text: string, image: Image | null = null): string {
+          return convertMarkDownNoFigure(text, image);
+      }
   }
 </script>
 
