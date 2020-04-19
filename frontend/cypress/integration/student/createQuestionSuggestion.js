40a32073dc1d010a83078bb0b8e439965d6f70f9
Gonçalo Freire <goncalo.b.freire@tecnico.ulisboa.pt>
2020-04-19 16:46:44 +0100

    Removed /cypress from gitignore and add questionSuggestion test %82

---
+++ frontend/cypress/integration/student/createQuestionSuggestion.js
@@ -0,0 +1,41 @@
+describe('Create a valide suggestion', () => {
+  beforeEach(() => {
+    cy.demoStudentLogin()
+  })
+
+  afterEach(() => {
+    cy.contains('Logout').click()
+  })
+
+  it('login and creates a suggestion and shows it', () => {
+    cy.createQuestionSuggestion('TestNormal','Question','a', 'b', 'c', 'd', 'No');
+  });
+
+  it('login and creates a Suggestion without title', () => {
+    cy.createQuestionSuggestion('','Question','a', 'b', 'c', 'd', 'No');
+    cy.closeErrorMessage();
+    cy.get('[data-cy="cancelButton"]').click()
+  });
+
+  it('login and creates a suggestions without question', () => {
+    cy.createQuestionSuggestion('TestNoQuestion','','a', 'b', 'c', 'd', 'No');
+    cy.closeErrorMessage();
+    cy.get('[data-cy="cancelButton"]').click()
+  });
+
+  it('login and creates a suggestions without one option', () => {
+    cy.createQuestionSuggestion('TestNoOption','Question','a', 'b', 'c', '', 'No');
+    cy.closeErrorMessage();
+    cy.get('[data-cy="cancelButton"]').click()
+  });
+
+  it('login and creates a suggestions without one option correct', () => {
+    cy.createQuestionSuggestion('TestNoCorrect','Question','a', 'b', 'c', 'd', 'Yes');
+    cy.closeErrorMessage();
+    cy.get('[data-cy="cancelButton"]').click()
+  });
+
+  it('Show created question', () => {
+    cy.showQuestionSuggestion('TestNormal')
+  })
+});
