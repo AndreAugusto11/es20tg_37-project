2f4d29ed36f4317b3860a769e62ac63757a1237b
Miguel Leve <miguel.levezinho@tecnico.ulisboa.pt>
2020-05-06 15:15:35 +0100

    Refactored Code for pull request

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/questionSuggestion/service/performanceTest/AcceptQuestionSuggestionPerformanceTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/questionSuggestion/service/performanceTest/AcceptQuestionSuggestionPerformanceTest.groovy
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecutionRepository
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Option
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.QuestionRepository
 import pt.ulisboa.tecnico.socialsoftware.tutor.questionSuggestion.QuestionSuggestionService
@@ -17,8 +19,10 @@ import spock.lang.Specification
 @DataJpaTest
 class AcceptQuestionSuggestionPerformanceTest extends Specification {
 
+    public static final String COURSE_NAME = "Software Architecture"
     public static final String QUESTION_TITLE = "title"
     public static final String QUESTION_CONTENT = "question content"
+    public static final String OPTION_CONTENT = "option content"
 
     @Autowired
     QuestionSuggestionService questionSuggestionService
@@ -38,16 +42,26 @@ class AcceptQuestionSuggestionPerformanceTest extends Specification {
     @Autowired
     UserRepository userRepository
 
-    def question
+    def course = new Course()
+    def question = new Question()
 
     def setup() {
-        question = new Question()
+        course.setName(COURSE_NAME)
+        course.setType(Course.Type.TECNICO)
+        courseRepository.save(course)
+
+        def option = new Option()
+        option.setContent(OPTION_CONTENT)
+        option.setCorrect(true)
+        option.setSequence(0)
+
         question.setKey(1)
         question.setTitle(QUESTION_TITLE)
         question.setContent(QUESTION_CONTENT)
+        question.addOption(option)
         question.setType(Question.Type.SUGGESTION)
         question.setStatus(Question.Status.DISABLED)
-        questionRepository.save(question)
+        question.setCourse(course)
     }
 
     def "Performance testing to accept 10000 question suggestions"() {
