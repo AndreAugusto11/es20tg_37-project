df6e57e921caa4bb2f1a0ff500f598b30e89740f
J. Galamba <jgalamba10@gmail.com>
2020-05-08 01:08:48 +0100

    Requested Changes for #161

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/performance/CancelTournamentPerformanceTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/performance/CancelTournamentPerformanceTest.groovy
@@ -4,10 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.AnswerService
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.AnswersXmlImport
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.QuestionService
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.TopicRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.QuizService
+import pt.ulisboa.tecnico.socialsoftware.tutor.statement.StatementService
 import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentService
 import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.domain.Tournament
 import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.repository.TournamentRepository
@@ -39,7 +44,7 @@ class CancelTournamentPerformanceTest extends Specification {
 
     def setup()
     {
-        maxTests = 10000
+        maxTests = 1
         user = new User("name", "username", 1, User.Role.STUDENT)
         userRepository.save(user)
 
@@ -69,6 +74,31 @@ class CancelTournamentPerformanceTest extends Specification {
 
     @TestConfiguration
     static class TournamentServiceImplTestContextConfiguration {
+
+        @Bean
+        QuestionService QuestionService() {
+            return new QuestionService()
+        }
+
+        @Bean
+        AnswersXmlImport AnswersXmlImport() {
+            return new AnswersXmlImport()
+        }
+
+        @Bean
+        QuizService QuizService() {
+            return new QuizService()
+        }
+
+        @Bean
+        AnswerService AnswerService() {
+            return new AnswerService()
+        }
+
+        @Bean
+        StatementService statementService() {
+            return new StatementService()
+        }
 
         @Bean
         TournamentService tournamentService() {
