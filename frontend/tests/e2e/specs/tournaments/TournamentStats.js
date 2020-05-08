f632d005a2bc73fe7ca2d3c041b2eba3bdc4abee
ChrisDragic <manuel.d.mascarenhas@tecnico.ulisboa.pt>
2020-05-08 11:51:01 +0100

    Fixed cypress and jmeter
    Tests insert had creator_id column that was replaced by user_id;

--- frontend/tests/e2e/specs/tournaments/TournamentStats.js
+++ frontend/tests/e2e/specs/tournaments/TournamentStats.js
@@ -6,7 +6,7 @@ describe('Show stats', () => {
     cy.exec('psql -d tutordb -c "Delete from tournaments_topics;"')
     cy.exec('psql -d tutordb -c "Delete from tournaments ;"')
     cy.demoStudentLogin();
-    cy.contains('Suggestions').click();
+    cy.contains('Tournaments').click();
   });
 
   afterEach(() => {
@@ -23,7 +23,7 @@ describe('Show stats', () => {
       .should('exist');
   });
   it('it enrolls in a Tournament and see the  stats', () => {
-    cy.exec('psql -d tutordb -c "insert into tournaments(id,end_date,number_of_questions,start_date,status,creator_id) values (1234,\'2020-05-26 00:00:00\',1,\'2020-05-24 00:00:00\',\'OPEN\',641);"')
+    cy.exec('psql -d tutordb -c "insert into tournaments(id,end_date,number_of_questions,start_date,status,user_id) values (1234,\'2020-05-26 00:00:00\',1,\'2020-05-24 00:00:00\',\'OPEN\',641);"')
     cy.exec('psql -d tutordb -c "insert into tournaments_topics(tournament_id,topics_id) values (1234,82);"')
     cy.enrollTournament('1234')
     cy.demoStudentLogin();
