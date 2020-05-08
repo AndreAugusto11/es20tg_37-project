a271e322d870750fa6c4682584a345d9ee705034
J. Galamba <jgalamba10@gmail.com>
2020-05-08 02:09:18 +0100

    Changed some PSQL Commands to the beforeEach and afterEach as they are repeated per test

--- frontend/tests/e2e/specs/tournaments/createTournaments.js
+++ frontend/tests/e2e/specs/tournaments/createTournaments.js
@@ -5,14 +5,15 @@ describe('Tournaments walkthrough', () => {
 
   afterEach(() => {
     cy.contains('Logout').click();
-  });
 
-  it('login and create a tournament', () => {
-    cy.createTournaments('5', 'GitHub', '2025-11-23 10:50', '2025-11-23 10:59');
     cy.exec('psql -d tutordb -c "DELETE FROM tournaments_users;"');
     cy.exec('psql -d tutordb -c "DELETE FROM users_tournaments;"');
     cy.exec('psql -d tutordb -c "DELETE FROM users_created_tournaments;"');
     cy.exec('psql -d tutordb -c "DELETE FROM tournaments_topics;"');
     cy.exec('psql -d tutordb -c "DELETE FROM tournaments;"');
+  });
+
+  it('login and create a tournament', () => {
+    cy.createTournaments('5', 'GitHub', '2025-11-23 10:50', '2025-11-23 10:59');
   });
 });
