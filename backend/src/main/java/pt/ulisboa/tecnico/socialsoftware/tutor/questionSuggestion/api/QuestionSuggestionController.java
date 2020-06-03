bbedad6f12f972aec4da12602d234e016b999273
Miguel Leve <miguel.levezinho@tecnico.ulisboa.pt>
2020-06-03 20:58:20 +0100

    issue #176 update
    - Added controller for attaching an image to a suggestion

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/questionSuggestion/api/QuestionSuggestionController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/questionSuggestion/api/QuestionSuggestionController.java
@@ -3,10 +3,12 @@ package pt.ulisboa.tecnico.socialsoftware.tutor.questionSuggestion.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto;
 import pt.ulisboa.tecnico.socialsoftware.tutor.questionSuggestion.QuestionSuggestionService;
@@ -16,8 +18,14 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.questionSuggestion.dto.QuestionSu
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 import java.util.List;
+import java.util.Objects;
 
 import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.AUTHENTICATION_ERROR;
 
@@ -28,6 +36,9 @@ public class QuestionSuggestionController {
 
     @Autowired
     private QuestionSuggestionService questionSuggestionService;
+
+    @Value("${figures.dir}")
+    private String figuresDir;
 
     @PostMapping("/courses/{courseId}/questionSuggestions")
     @PreAuthorize("hasRole('ROLE_STUDENT') and hasPermission(#courseId, 'COURSE.ACCESS')")
@@ -92,11 +103,38 @@ public class QuestionSuggestionController {
         return questionSuggestionService.updateRejectedQuestionSuggestion(questionSuggestionId, questionSuggestionDto);
     }
 
+    @PutMapping("/questionSuggestions/{questionSuggestionId}/image")
+    @PreAuthorize("hasRole('ROLE_TEACHER') and hasPermission(#questionId, 'QUESTION.ACCESS')")
+    public String uploadImage(@PathVariable Integer questionSuggestionId, @RequestParam("file") MultipartFile file) throws IOException {
+        logger.debug("uploadImage  questionId: {}: , filename: {}", questionSuggestionId, file.getContentType());
+
+        QuestionDto questionDto = questionSuggestionService.findQuestionSuggestionById(questionSuggestionId).getQuestionDto();
+        String url = questionDto.getImage() != null ? questionDto.getImage().getUrl() : null;
+        if (url != null && Files.exists(getTargetLocation(url))) {
+            Files.delete(getTargetLocation(url));
+        }
+
+        int lastIndex = Objects.requireNonNull(file.getContentType()).lastIndexOf('/');
+        String type = file.getContentType().substring(lastIndex + 1);
+
+        questionSuggestionService.uploadImage(questionSuggestionId, type);
+
+        url = questionSuggestionService.findQuestionSuggestionById(questionSuggestionId).getImage().getUrl();
+        Files.copy(file.getInputStream(), getTargetLocation(url), StandardCopyOption.REPLACE_EXISTING);
+
+        return url;
+    }
+
     @DeleteMapping("/questionSuggestions/{questionSuggestionId}")
     @PreAuthorize("hasRole('ROLE_ADMIN') or (hasRole('ROLE_DEMO_ADMIN'))")
     public ResponseEntity removeQuestionSuggestion(@PathVariable Integer questionSuggestionId) {
         questionSuggestionService.removeQuestionSuggestion(questionSuggestionId);
 
         return ResponseEntity.ok().build();
+    }
+
+    private Path getTargetLocation(String url) {
+        String fileLocation = figuresDir + url;
+        return Paths.get(fileLocation);
     }
 }
