ae9e11d58a4422aec392888ad70bef6e28872d83
Miguel Leve <miguel.levezinho@tecnico.ulisboa.pt>
2020-06-07 01:21:42 +0100

    issue #179 and #189 update
    - Added 7 new tests for cancelTournament service

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/CancelTournamentTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/service/CancelTournamentTest.groovy
@@ -1,14 +1,14 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.tournament.service
 
 import pt.ulisboa.tecnico.socialsoftware.tutor.answer.AnswerService
+import pt.ulisboa.tecnico.socialsoftware.tutor.config.DateHandler
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecution
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecutionRepository
 import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
-import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
 import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.AnswersXmlImport
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.QuestionService
-import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic
-import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.TopicRepository
 import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.QuizService
 import pt.ulisboa.tecnico.socialsoftware.tutor.statement.StatementService
 import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.domain.Tournament
@@ -22,15 +22,35 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
 import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.TournamentService
 import pt.ulisboa.tecnico.socialsoftware.tutor.tournament.repository.TournamentRepository
+import spock.lang.Unroll
 
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.TOURNAMENT_NON_CREATOR
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.TOURNAMENT_NOT_ENROLLING
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.TOURNAMENT_NOT_FOUND
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.USER_NOT_FOUND
 
 @DataJpaTest
-class CancelTournamentTest extends Specification{
+class CancelTournamentTest extends Specification {
+
+    public static final String COURSE_NAME = "Software Architecture"
+    public static final String COURSE_ACRONYM = "SA1"
+    public static final String ACADEMIC_TERM = "First Semester"
+    public static final String CREATOR_NAME = "Creator Name"
+    public static final String CREATOR_USERNAME = "Creator Username"
+    public static final String ENROLLER_NAME = "Enroller Name"
+    public static final String ENROLLER_USERNAME = "Enroller Username"
+    public static final String TOURNAMENT_TITLE = "Tournament Title"
+    public static final String AVAILABLE_DATE = "2020-01-25T16:30:11Z"
+    public static final String CONCLUSION_DATE = "2020-01-25T17:40:11Z"
 
     @Autowired
     TournamentService tournamentService
+
+    @Autowired
+    CourseRepository courseRepository
+
+    @Autowired
+    CourseExecutionRepository courseExecutionRepository
 
     @Autowired
     TournamentRepository tournamentRepository
@@ -38,115 +58,98 @@ class CancelTournamentTest extends Specification{
     @Autowired
     UserRepository userRepository
 
-    @Autowired
-    TopicRepository topicRepository
+    def course = new Course()
+    def courseExecution = new CourseExecution()
+    def creator = new User()
+    def enroller = new User()
+    def tournament = new Tournament()
 
-    @Autowired
-    CourseRepository courseRepository
-
-    def user
-    def tournament
-    def userID
-    def tournamentId
-
-    def setup()
-    {
-        user = new User('student', "istStu", 1, User.Role.STUDENT)
-        userRepository.save(user)
-        user = userRepository.findByKey(1)
-
-        def topicOne = new Topic()
-        topicOne.setName("TOPIC1")
-        def topicTwo = new Topic()
-        topicTwo.setName("TOPIC2")
-        def topicThree = new Topic()
-        topicThree.setName("TOPIC3")
-
-        def course = new Course("LEIC", Course.Type.TECNICO)
+    def setup() {
+        course.setName(COURSE_NAME)
+        course.setType(Course.Type.TECNICO)
         courseRepository.save(course)
-        course = courseRepository.findByNameType("LEIC", "TECNICO").get()
-        topicOne.setCourse(course)
-        topicTwo.setCourse(course)
-        topicThree.setCourse(course)
 
-        def topics = new HashSet<Topic>()
-        topics.add(topicOne)
-        topics.add(topicTwo)
-        topics.add(topicThree)
+        courseExecution.setCourse(course)
+        courseExecution.setType(Course.Type.TECNICO)
+        courseExecution.setAcronym(COURSE_ACRONYM)
+        courseExecution.setAcademicTerm(ACADEMIC_TERM)
+        courseExecutionRepository.save(courseExecution)
 
-        topicRepository.save(topicOne)
-        topicRepository.save(topicTwo)
-        topicRepository.save(topicThree)
+        creator.setName(CREATOR_NAME)
+        creator.setUsername(CREATOR_USERNAME)
+        creator.setKey(1)
+        creator.setRole(User.Role.STUDENT)
+        creator.addCourseExecutions(courseExecution)
+        userRepository.save(creator)
 
-        def formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm")
-        def startTime = LocalDateTime.now().plusDays(1)
-        startTime.format(formatter)
-        def endTime = LocalDateTime.now().plusDays(2)
-        endTime.format(formatter)
+        enroller.setName(ENROLLER_NAME)
+        enroller.setUsername(ENROLLER_USERNAME)
+        enroller.setKey(2)
+        enroller.setRole(User.Role.STUDENT)
+        enroller.addCourseExecutions(courseExecution)
+        userRepository.save(enroller)
 
-        tournament = new Tournament(user, topics, 5, startTime, endTime)
+        tournament.setTitle(TOURNAMENT_TITLE)
+        tournament.setCourseExecution(courseExecution)
+        tournament.setCreator(creator)
+        tournament.setAvailableDate(DateHandler.toLocalDateTime(AVAILABLE_DATE))
+        tournament.setConclusionDate(DateHandler.toLocalDateTime(CONCLUSION_DATE))
+        tournament.setStatus(Tournament.Status.ENROLLING)
         tournamentRepository.save(tournament)
-        tournamentId = tournament.getId()
-        userID = user.getId()
     }
 
-    def "cancel a tournament previously created"()
-    {
-        when:
-        tournamentService.cancelTournament(userID, tournamentId)
-        then: "the tournament is removed"
-        def t = tournamentRepository.findById(tournamentId).get()
-        t.getCreator().getId() == userID
-        t.getStatus() == Tournament.Status.CANCELLED
+    def "The creator of a tournament cancels a created tournament"() {
+        when: "the creator tries to cancel"
+        tournamentService.cancelTournament(creator.getId(), tournament.getId())
+
+        then: "the tournament becomes cancelled"
+        tournament.getStatus() == Tournament.Status.CANCELLED
     }
 
-    def "tournament was already cancelled"()
-    {
-        given: "a cancelled tournament"
-        tournamentService.cancelTournament(userID,tournamentId)
-        when:
-        tournamentService.cancelTournament(userID,tournamentId)
+    def "Cannot cancel a tournament given an invalid user id"() {
+        when: "the creator tries to cancel"
+        tournamentService.cancelTournament(0, tournament.getId())
 
-        then:
+        then: "an exception is thrown"
         def exception = thrown(TutorException)
-        exception.getErrorMessage() == ErrorMessage.TOURNAMENT_ALREADY_CANCELLED
+        exception.getErrorMessage() == USER_NOT_FOUND
     }
 
-    def "non-valid user"()
-    {
-        given: "a user ID"
-        def userID = 0
+    def "Cannot cancel a tournament given an invalid tournament id"() {
+        when: "the creator tries to cancel"
+        tournamentService.enrollInTournament(creator.getId(), 0)
 
-        when:
-        tournamentService.cancelTournament(userID, tournamentId)
-        then:
+        then: "an exception is thrown"
         def exception = thrown(TutorException)
-        exception.getErrorMessage() == ErrorMessage.USER_NOT_FOUND
+        exception.getErrorMessage() == TOURNAMENT_NOT_FOUND
     }
 
-    def "tournament not created by the user"()
-    {
-        given: "a user ID"
-        def newStudent = new User("name", "userN", 2, User.Role.STUDENT)
-        userRepository.save(newStudent)
-        def userID = userRepository.findByKey(2).getId()
+    def "Cannot cancel a tournament given an user that is not the tournament creator"() {
+        when: "a student not creator tries to cancel"
+        tournamentService.cancelTournament(enroller.getId(), tournament.getId())
 
-        when:
-        tournamentService.cancelTournament(userID, tournamentId)
-        then:
+        then: "an exception is thrown"
         def exception = thrown(TutorException)
-        exception.getErrorMessage() == ErrorMessage.TOURNAMENT_NON_CREATOR
+        exception.getErrorMessage() == TOURNAMENT_NON_CREATOR
     }
 
-    def "tournament ID is invalid"()
-    {
-        given: "and invalid Tournament ID"
-        def tID = 0
-        when:
-        tournamentService.cancelTournament(userID, tID)
-        then:
+    @Unroll
+    def "Cannot cancel a tournament that is #test"() {
+        given: "a tournament that is #test"
+        tournament.setStatus(status)
+
+        when: "the creator tries to cancel"
+        tournamentService.cancelTournament(creator.getId(), tournament.getId())
+
+        then: "an exception is thrown"
         def exception = thrown(TutorException)
-        exception.getErrorMessage() == ErrorMessage.TOURNAMENT_NOT_FOUND
+        exception.getErrorMessage() == errorMessage
+
+        where:
+        test        | status                      || errorMessage
+        "ongoing"   | Tournament.Status.ONGOING   || TOURNAMENT_NOT_ENROLLING
+        "concluded" | Tournament.Status.CONCLUDED || TOURNAMENT_NOT_ENROLLING
+        "cancelled" | Tournament.Status.CANCELLED || TOURNAMENT_NOT_ENROLLING
     }
 
     @TestConfiguration
