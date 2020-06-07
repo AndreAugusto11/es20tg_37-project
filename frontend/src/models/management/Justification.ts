9ce4f17fe9e84f11bad752a999ff4fa88abd7d2a
AndreAugusto <andre.9a@gmail.com>
2020-06-08 00:26:54 +0100

    Add new buttons to reject and accept Suggestions

--- frontend/src/models/management/Justification.ts
+++ frontend/src/models/management/Justification.ts
@@ -4,12 +4,14 @@ export default class Justification {
   id: number | null = null;
   image: Image | null = null;
   content: string = '';
+  name: string | null = null;
 
   constructor(jsonObj?: Justification) {
     if (jsonObj) {
       this.id = jsonObj.id;
       this.image = jsonObj.image;
       this.content = jsonObj.content;
+      this.name = jsonObj.name;
     }
   }
 }
--- frontend/src/views/questionSuggestions/QuestionSuggestionView.vue
+++ frontend/src/views/questionSuggestions/QuestionSuggestionView.vue
@@ -1,10 +1,128 @@
 <template>
-  <div>
-    <v-card class="mx-auto mt-10 mb-10" max-width="1100" outlined>
-      <!--<v-card-title class="mb-2">
-        <span class="headline">{{ questionSuggestion.questionDto.title }}</span>
-      </v-card-title>-->
+  <div :key="this.update">
+    <v-card
+      class="mx-auto mt-8 pb-3"
+      max-width="1100"
+      outlined
+      color="#eeeeee"
+      v-if="questionSuggestion.justificationDto"
+    >
+      <v-row align="center" class="spacer ml-5 mt-5 mr-5" no-gutters>
+        <v-col md="auto" class="mr-5">
+          <v-avatar
+            :color="this.getRandomVuetifyColor(this.questionSuggestion.justificationDto.name)"
+          >
+            <span
+              class="white--text headline"
+            >{{ this.getNameInitials(this.questionSuggestion.justificationDto.name) }}</span>
+          </v-avatar>
+        </v-col>
 
+        <v-col sm="3" md="7">
+          <h2 class="mb-1 text-left">{{ this.questionSuggestion.justificationDto.name }}</h2>
+        </v-col>
+      </v-row>
+
+      <v-row align="center" class="spacer" no-gutters>
+        <v-col>
+          <v-subheader>Justification</v-subheader>
+        </v-col>
+      </v-row>
+      <v-row align="center" class="spacer ml-7 mr-7" no-gutters>
+        <v-col class="text-left">
+          <span
+            class="text-justify"
+            v-html="convertMarkDown(questionSuggestion.justificationDto.content, null)"
+          />
+        </v-col>
+      </v-row>
+
+      <v-card-text v-if="questionSuggestion.justificationDto.image">
+        <span
+          v-html="convertMarkDown('![image][image]', questionSuggestion.justificationDto.image)"
+        />
+      </v-card-text>
+
+      <v-container v-if="this.editing" grid-list-md fluid>
+        <v-layout column wrap>
+          <v-flex xs24 sm12 md8>
+            <v-textarea
+              class="ml-3 mr-3"
+              outline
+              rows="3"
+              v-model="justification.content"
+              label="New Justification Content"
+              data-cy="Content"
+            ></v-textarea>
+          </v-flex>
+        </v-layout>
+
+        <template>
+          <v-file-input
+            class="pr-3"
+            label="File input"
+            show-size
+            outlined
+            counter
+            dense
+            @change="constructImage($event)"
+            accept="image/*"
+          />
+        </template>
+      </v-container>
+
+      <v-card-actions v-if="this.editing">
+        <v-spacer />
+        <v-btn dark color="blue darken-1" @click="this.startEditing">close</v-btn>
+        <v-btn dark color="blue darken-1" @click="saveJustification">save</v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <!-- New Card -->
+
+    <v-card
+      class="mx-auto mt-8 pb-3"
+      max-width="1100"
+      outlined
+      color="#eeeeee"
+      v-if="this.editing && !this.questionSuggestion.justificationDto"
+    >
+      <v-container v-if="this.editing" grid-list-md fluid>
+        <v-layout column wrap>
+          <v-flex xs24 sm12 md8>
+            <v-textarea
+              class="ml-3 mr-3"
+              outline
+              rows="3"
+              v-model="justification.content"
+              label="New Justification Content"
+              data-cy="Content"
+            ></v-textarea>
+          </v-flex>
+        </v-layout>
+
+        <template>
+          <v-file-input
+            class="pr-3"
+            label="File input"
+            show-size
+            outlined
+            counter
+            dense
+            @change="constructImage($event)"
+            accept="image/*"
+          />
+        </template>
+      </v-container>
+
+      <v-card-actions v-if="this.editing">
+        <v-spacer />
+        <v-btn dark color="blue darken-1" @click="this.startEditing">close</v-btn>
+        <v-btn dark color="blue darken-1" @click="saveJustification">save</v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <v-card class="mx-auto mb-10 mt-10 pt-2" max-width="1100" outlined>
       <v-row align="center" class="spacer ml-5 mr-5 mt-5" no-gutters>
         <v-col md="auto" class="mr-5">
           <v-avatar :color="this.getRandomVuetifyColor(this.questionSuggestion.name)">
@@ -28,6 +146,31 @@
       <v-card-text class="mt-3" v-if="this.questionSuggestion.questionDto.image">
         <span v-html="convertMarkDown('![image][image]', questionSuggestion.questionDto.image)" />
       </v-card-text>
+
+      <v-btn
+        v-if="questionSuggestion.status === 'PENDING' && !this.editing"
+        absolute
+        fab
+        top
+        right
+        color="green"
+        class="buttonAccept"
+        @click="acceptSuggestion"
+      >
+        <v-icon class="pl-1" color="white">mdi-check</v-icon>
+      </v-btn>
+
+      <v-btn
+        v-if="questionSuggestion.status === 'PENDING' && !this.editing"
+        absolute
+        fab
+        top
+        right
+        color="red"
+        @click="this.startEditing"
+      >
+        <v-icon class="pl-1" color="white">mdi-close</v-icon>
+      </v-btn>
 
       <v-divider v-if="this.questionSuggestion.questionDto.image"></v-divider>
 
@@ -74,27 +217,6 @@
         </v-list>
       </v-card-text>
     </v-card>
-
-    <v-card
-      class="mx-auto mb-10 pb-3"
-      max-width="1100"
-      outlined
-      v-if="questionSuggestion.justificationDto"
-    >
-      <v-card-title>
-        <span class="headline">Justification</span>
-      </v-card-title>
-
-      <v-card-text class="text-justify">
-        <span v-html="convertMarkDown(questionSuggestion.justificationDto.content, null)" />
-      </v-card-text>
-
-      <v-card-text v-if="questionSuggestion.justificationDto.image">
-        <span
-          v-html="convertMarkDown('![image][image]', questionSuggestion.justificationDto.image)"
-        />
-      </v-card-text>
-    </v-card>
   </div>
 </template>
 
@@ -105,15 +227,22 @@ import QuestionSuggestion from '@/models/management/QuestionSuggestion';
 import Image from '@/models/management/Image';
 import { getNameInitials } from '@/services/GetNameInitialsService';
 import { getRandomVuetifyColor } from '@/services/GetRandomVuetifyColorService';
+import RemoteServices from '@/services/RemoteServices';
+import Justification from '../../models/management/Justification';
 
 @Component
 export default class QuestionSuggestionView extends Vue {
   questionSuggestion!: QuestionSuggestion;
+  justification!: Justification;
+  editing: boolean = false;
+  update: number = 1;
+  file!: File;
 
   async created() {
     this.questionSuggestion = new QuestionSuggestion(
       JSON.parse(this.$route.params.questionSuggestion)
     );
+    this.justification = new Justification();
   }
 
   convertMarkDown(text: string, image: Image | null = null): string {
@@ -133,6 +262,70 @@ export default class QuestionSuggestionView extends Vue {
   getRandomVuetifyColor(name: string): string {
     return getRandomVuetifyColor(name);
   }
+
+  startEditing() {
+    this.editing = this.editing ? false : true;
+  }
+
+  constructImage(event: File) {
+    this.file = event;
+  }
+
+  async acceptSuggestion() {
+    try {
+      if (this.questionSuggestion.id) {
+        await RemoteServices.acceptQuestionSuggestion(
+          this.questionSuggestion.id
+        );
+        this.questionSuggestion.status = 'ACCEPTED';
+      }
+    } catch (error) {
+      await this.$store.dispatch('error', error);
+    }
+    this.update++;
+  }
+
+  async saveJustification() {
+    if (this.justification && !this.justification.content) {
+      await this.$store.dispatch('error', 'Justification must have content');
+    } else {
+      try {
+        if (this.questionSuggestion.id) {
+          await RemoteServices.rejectQuestionSuggestion(
+            this.questionSuggestion.id,
+            this.justification
+          );
+
+          if (this.questionSuggestion.justificationDto) {
+            this.questionSuggestion.justificationDto.content = this.justification.content;
+          }
+          else {
+            this.questionSuggestion.justificationDto = this.justification;
+            this.questionSuggestion.justificationDto.name = this.$store.getters.getUser.name
+          }        
+
+          this.questionSuggestion.status = 'REJECTED';
+
+          if (this.file) {
+            let url = await RemoteServices.uploadImageToJustification(
+              this.questionSuggestion.id,
+              this.file
+            );
+            let image = new Image();
+            image.url = url;
+
+            this.questionSuggestion.justificationDto.image = image;
+          } else {
+            this.questionSuggestion.justificationDto.image = null;
+          }
+          this.update++;
+          this.editing = false;
+        }
+      } catch (error) {
+        await this.$store.dispatch('error', error);
+      }
+    }
+  }
 }
 </script>
 
@@ -145,5 +338,9 @@ img {
 
 .v-application p {
   margin-bottom: 0 !important;
+}
+
+.buttonAccept {
+  margin-right: 6%;
 }
 </style>
